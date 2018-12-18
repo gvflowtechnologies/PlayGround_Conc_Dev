738213f7ba947390073a5b154c32a05b3eb63125
Gregory Voss <gvoss@tflow.net>
2018-12-17 20:22:30 -0600

    Added Events form closing and form load

--- Concnetrator Tether/Form1.vb
+++ Concnetrator Tether/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Form1_Close(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+
+    End Sub
+
+    Private Sub Form1_FOrmCLosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+    End Sub
 End Class
