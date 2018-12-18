418261df426179692c026d56189ec73eedc6aec3
Gregory Voss <gvoss@tflow.net>
2018-12-17 20:45:36 -0600

    added module to turn off sleep

--- Concnetrator Tether/Form1.vb
+++ Concnetrator Tether/Form1.vb
@@ -1,13 +1,27 @@
-﻿Public Class Form1
+﻿Imports System.IO.Ports
+Imports System.Threading
+
+
+Public Class Form1
+
+
+
+    Private Delegate Sub accessformMarshaldelegate(ByVal texttodisplay As String)
+    Public WithEvents mycom As SerialPort
+
+    Const timerperiod As Integer = 5 ' 5 millisecond time ISR period on arduino
+
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Interop.No_Sleep()
+
+
 
     End Sub
 
-    Private Sub Form1_Close(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
 
-    End Sub
 
     Private Sub Form1_FOrmCLosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-
+        Interop.GOTOSLEEP()
     End Sub
 End Class
---
+++ Concnetrator Tether/Interop.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Runtime.InteropServices
+
+Module Interop
+    Public Enum EXECUTION_STATE As UInteger ' Define the API Execution states
+        ES_SYSTEM_REQUIRED = &H1
+        ES_DISPLAY_REQUIRED = &H2
+        ES_CONTINUOUS = &H80000000UI
+    End Enum
+
+    Private Declare Function SetThreadExecutionState Lib "Kernel32" (ByVal esflags As EXECUTION_STATE) As EXECUTION_STATE
+
+    Function No_Sleep() As EXECUTION_STATE
+        Return SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED Or EXECUTION_STATE.ES_DISPLAY_REQUIRED Or EXECUTION_STATE.ES_CONTINUOUS)
+    End Function
+    Function GOTOSLEEP() As EXECUTION_STATE
+        Return SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS)
+    End Function
+
+
+
+End Module
